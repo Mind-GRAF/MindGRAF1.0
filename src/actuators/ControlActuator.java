8e1f554512fc43d4606837a58768ed2cc202d4d1
marwannelsayed <marwanyehiaelsayed@gmail.com>
2024-03-27 14:23:43 +0200

    Added Control Actuators

---
+++ src/actuators/ControlActuator.java
@@ -0,0 +1,7 @@
+package actuators;
+
+import nodes.ActNode;
+
+public interface ControlActuator {
+    void actOnNode(ActNode node);
+}
---
+++ src/actuators/DoAllActuator.java
@@ -0,0 +1,12 @@
+package actuators;
+
+import nodes.ActNode;
+
+public class DoAllActuator implements ControlActuator{
+
+    @Override
+    public void actOnNode(ActNode node) {
+        // TODO Auto-generated method stub
+    }
+    
+}
