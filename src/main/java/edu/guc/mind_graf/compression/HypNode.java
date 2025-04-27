304bc428a425d9aedab5ff25caea17ceafd46064
NadaShetewi <161207170+NadaShetewi@users.noreply.github.com>
2025-04-27 19:00:14 +0300

    toString for debugging issues

--- src/main/java/edu/guc/mind_graf/compression/HypNode.java
+++ src/main/java/edu/guc/mind_graf/compression/HypNode.java
@@ -2,31 +2,44 @@ package edu.guc.mind_graf.compression;
 
 import java.util.Objects;
 
-public class HypNode implements Comparable{
+public class HypNode implements Comparable {
     int nodeID;
     int attitudeID;
-    public HypNode(int nodeID, int attitudeID){
-        this.nodeID = nodeID;
-        this.attitudeID = attitudeID; 
-    }
 
+    public HypNode(int nodeID, int attitudeID) {
+        this.nodeID = nodeID;
+        this.attitudeID = attitudeID;
+    }
 
     @Override
     public int compareTo(Object arg0) {
         HypNode other = (HypNode) arg0;
-        if(other.nodeID == nodeID && other.attitudeID == attitudeID){
+        if (other.nodeID == nodeID && other.attitudeID == attitudeID) {
             return 0;
         }
         return -1;
     }
+
     public boolean equals(Object obj) {
-    if (this == obj) return true;
-    if (obj == null || getClass() != obj.getClass()) return false;
-    HypNode myClass = (HypNode) obj;
-    return Objects.equals(nodeID, myClass.nodeID) &&
-           Objects.equals(attitudeID, myClass.attitudeID);
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        HypNode myClass = (HypNode) obj;
+        return Objects.equals(nodeID, myClass.nodeID) &&
+                Objects.equals(attitudeID, myClass.attitudeID);
     }
+
     public int hashCode() {
         return Objects.hash(nodeID, attitudeID);
     }
+
+    @Override
+    public String toString() {
+        return "HypNode{" +
+                "nodeID=" + nodeID +
+                ", attitudeID=" + attitudeID +
+                '}';
+    }
+
 }
--- src/main/java/edu/guc/mind_graf/compression/SupportNode.java
+++ src/main/java/edu/guc/mind_graf/compression/SupportNode.java
@@ -7,13 +7,14 @@ import java.util.Objects;
 public class SupportNode {
     private ArrayList<HypNode> supportingNodes;
 
-    public SupportNode(ArrayList<HypNode> hypNodes, HypNode... hypNode){
+    public SupportNode(ArrayList<HypNode> hypNodes, HypNode... hypNode) {
         this.supportingNodes = new ArrayList<HypNode>();
         this.supportingNodes.addAll(hypNodes);
         this.supportingNodes.addAll(java.util.Arrays.asList(hypNode));
     }
+
     public SupportNode() {
-        supportingNodes =  new ArrayList<HypNode>();
+        supportingNodes = new ArrayList<HypNode>();
 
     }
 
@@ -25,17 +26,29 @@ public class SupportNode {
     public SupportNode(HypNode... hypNode) {
         this.supportingNodes = new ArrayList<HypNode>(java.util.Arrays.asList(hypNode));
     }
-    public ArrayList<HypNode> getSupportingNodes(){
+
+    public ArrayList<HypNode> getSupportingNodes() {
         return supportingNodes;
     }
+
     public boolean equals(Object obj) {
-    if (this == obj) return true;
-    if (obj == null || getClass() != obj.getClass()) return false;
-    SupportNode other = (SupportNode) obj;
-    return  Objects.equals(supportingNodes, other.supportingNodes);
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        SupportNode other = (SupportNode) obj;
+        return Objects.equals(supportingNodes, other.supportingNodes);
     }
+
     public int hashCode() {
         return Objects.hash(supportingNodes);
+    }
+
+    @Override
+    public String toString() {
+        return "SupportNode{" +
+                "supportingNodes=" + supportingNodes +
+                '}';
     }
 
 }
